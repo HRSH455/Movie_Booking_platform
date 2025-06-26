4b065d93bf5ab29f31b4a0f35a146ef7a5cccfae
neojarvis <techadmin@iamneo.ai>
2025-06-26 07:10:29 +0000

    Manual-save-commit-6/26/2025--12:39:57-PM

--- springapp/src/main/java/com/examly/springapp/controller/TestController.java
+++ springapp/src/main/java/com/examly/springapp/controller/TestController.java
@@ -1,9 +1,14 @@
 package com.examly.springapp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.examly.springapp.entity.Movie;
 
 @RestController
 public class TestController {
@@ -16,7 +21,13 @@ public class TestController {
 
     @GetMapping("api/test/movie")
     public ResponseEntity<?>getAll(){
-        List<
+        List<Movie> li = new ArrayList<>();
+        
+        li.add(new Movie(1l,"Don",120,"thriller",200));
+        li.add(new Movie(2l,"Donreturns",193,"RomCom",210));
+        li.add(new Movie(3l,"Donret",165,"Crime",120));
+        li.add(new Movie(4l,"Don2",124,"Comedy",150));
+        li.add(new Movie(5l,"Don4",150,"Dramedy",204));
     }
 
 }
