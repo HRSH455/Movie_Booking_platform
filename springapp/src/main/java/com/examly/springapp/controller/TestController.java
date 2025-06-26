e5d5798e8fbc95bb8c27a0701aeb4e808831962a
neojarvis <techadmin@iamneo.ai>
2025-06-26 07:02:09 +0000

    Auto-save-commit-6/26/2025--12:31:36-PM

--- springapp/src/main/java/com/examly/springapp/controller/TestController.java
+++ springapp/src/main/java/com/examly/springapp/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.examly.springapp.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ public class TestController {
     @GetMapping("/api/test/welcome")
     public String Welcome(){
         return "Welcome to the Movie Booking Application";
+    }
+
+    @GetMapping("api/test/movie")
+    public ResponseEntity<?>getAll(){
+        List<
     }
 
 }
