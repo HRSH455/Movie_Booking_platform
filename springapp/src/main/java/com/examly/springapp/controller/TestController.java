432651b72d7bb56c27c71bbd58cafb721b6f9049
neojarvis <techadmin@iamneo.ai>
2025-06-26 06:54:56 +0000

    Manual-save-commit-6/26/2025--12:24:23-PM

--- springapp/src/main/java/com/examly/springapp/controller/TestController.java
+++ springapp/src/main/java/com/examly/springapp/controller/TestController.java
@@ -2,14 +2,15 @@ package com.examly.springapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+@RestController
 public class TestController {
 
 
     @GetMapping("/api/test/welcome")
     public String Welcome(){
-        return "Welcome";
+        return "Welcome to the Movie Booking Application";
     }
 
 }
