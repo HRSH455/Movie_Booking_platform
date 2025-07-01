3a7487bc5e3dfaaad05e14df2cf07aac444f4b8e
neojarvis <techadmin@iamneo.ai>
2025-07-01 06:21:43 +0000

    Auto-save-commit-7/1/2025--11:51:05-AM

--- springapp/src/main/java/com/examly/springapp/entity/Booking.java
+++ springapp/src/main/java/com/examly/springapp/entity/Booking.java
@@ -1,5 +1,60 @@
 package com.examly.springapp.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long bookingId;
+    private int seatCount;
+    private double totalCost;
+
+    @ManyToOne
+    @JoinColumn(name = "movieId")
+    private Movie movie;
+
+    public Booking() {
+    }
+
+    public Booking(long bookingId, int seatCount, double totalCost, Movie movie) {
+        this.bookingId = bookingId;
+        this.seatCount = seatCount;
+        this.totalCost = totalCost;
+        this.movie = movie;
+    }
+
+    public long getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(long bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public int getSeatCount() {
+        return seatCount;
+    }
+
+    public void setSeatCount(int seatCount) {
+        this.seatCount = seatCount;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+        
     
 }
--- springapp/src/main/java/com/examly/springapp/entity/Movie.java
+++ springapp/src/main/java/com/examly/springapp/entity/Movie.java
@@ -1,10 +1,13 @@
 package com.examly.springapp.entity;
 
-import jakarta.annotation.Generated;
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Movie {
@@ -15,16 +18,20 @@ public class Movie {
     private int duration;
     private String genre;
     private int price;
-    
+
+    @OneToMany(mappedBy = "movie" , cascade = CascadeType.ALL ,orphanRemoval = true)
+    private List<Booking> bookings = new ArrayList<>();
+
     public Movie() {
     }
 
-    public Movie(long movieId, String title, int duration, String genre, int price) {
+    public Movie(long movieId, String title, int duration, String genre, int price, List<Booking> bookings) {
         this.movieId = movieId;
         this.title = title;
         this.duration = duration;
         this.genre = genre;
         this.price = price;
+        this.bookings = bookings;
     }
 
     public long getMovieId() {
@@ -66,6 +73,16 @@ public class Movie {
     public void setPrice(int price) {
         this.price = price;
     }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
     
     
+
+        
 }
