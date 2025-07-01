6487bdbbcd47b3ef8cedc4ea0c0cf966dc66f3ad
neojarvis <techadmin@iamneo.ai>
2025-07-01 05:51:41 +0000

    Auto-save-commit-7/1/2025--11:21:05-AM

--- springapp/src/main/java/com/examly/springapp/controller/MovieController.java
+++ springapp/src/main/java/com/examly/springapp/controller/MovieController.java
@@ -3,6 +3,7 @@ package com.examly.springapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -32,5 +33,6 @@ public class MovieController {
     {
         return movieService.updateById(movieId,m);
     }
+    @GetMapping("")
 
 }
