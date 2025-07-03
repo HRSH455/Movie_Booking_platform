be0bdcc8564837c408e35e1ff56892c109b674fa
neojarvis <techadmin@iamneo.ai>
2025-07-03 12:54:39 +0000

    Manual-save-commit-7/3/2025--6:23:49-PM

---
+++ springapp/src/main/java/com/examly/springapp/service/UserService.java
@@ -0,0 +1,11 @@
+package com.examly.springapp.service;
+
+import java.util.List;
+
+import com.examly.springapp.entity.User;
+
+public interface UserService {
+    User registerUser(User user);
+    List<User> getAllUser();
+    User loginUser(User user);
+}
---
+++ springapp/src/main/java/com/examly/springapp/service/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.examly.springapp.service.serviceImpl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.examly.springapp.entity.User;
+
+@Service
+public class UserServiceImpl {
+    public User registerUser(User user){
+        return null;
+    }
+    public List<User> getAllUser(){
+        return null;
+    }
+    public User loginUser(User user){
+        return null;
+    }
+}
