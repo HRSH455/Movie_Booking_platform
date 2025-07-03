c01b0ca2504a7bc8eb805fca96e12c00080ada27
neojarvis <techadmin@iamneo.ai>
2025-07-03 13:13:11 +0000

    Auto-save-commit-7/3/2025--6:42:21-PM

--- springapp/src/main/java/com/examly/springapp/controller/UserController.java
+++ springapp/src/main/java/com/examly/springapp/controller/UserController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     
-    @PostMapping("/api/user/register")
-    public new ResponseEntity<?>
+    //@PostMapping("/api/user/register")
+   //public ResponseEntity<?>
 }
