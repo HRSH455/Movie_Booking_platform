9f5ede41b0da5e6700363ec5079899e9dcd04d04
neojarvis <techadmin@iamneo.ai>
2025-07-03 13:13:41 +0000

    Manual-save-commit-7/3/2025--6:43:01-PM

--- springapp/src/main/java/com/examly/springapp/configuration/CorsConfig.java
+++ springapp/src/main/java/com/examly/springapp/configuration/CorsConfig.java
@@ -7,12 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@
+
 public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
         .allowedOrigins("*")
         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-        .all
+        .allowedHeaders("*");
     }
 }
