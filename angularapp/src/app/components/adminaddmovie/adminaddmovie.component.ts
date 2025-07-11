81fb26b52b9544343e05ff9a5259ee006707b8a9
neojarvis <techadmin@iamneo.ai>
2025-07-11 04:06:29 +0000

    Auto-save-commit-7/11/2025--9:35:44-AM

---
+++ angularapp/src/app/components/adminaddmovie/adminaddmovie.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adminaddmovie',
+  templateUrl: './adminaddmovie.component.html',
+  styleUrls: ['./adminaddmovie.component.css']
+})
+export class AdminaddmovieComponent implements OnInit {
+  movie ={
+    
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
