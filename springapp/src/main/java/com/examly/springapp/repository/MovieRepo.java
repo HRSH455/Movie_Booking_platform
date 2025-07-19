5410f7e276a50327e4fccf621f7c47b393b8b2ce
neojarvis <techadmin@iamneo.ai>
2025-07-19 09:31:43 +0000

    Auto-save-commit-7/19/2025--3:00:40-PM

--- springapp/src/main/java/com/examly/springapp/repository/MovieRepo.java
+++ springapp/src/main/java/com/examly/springapp/repository/MovieRepo.java
@@ -1,11 +1,16 @@
 package com.examly.springapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.examly.springapp.entity.Booking;
 import com.examly.springapp.entity.Movie;
 
 @Repository
 public interface MovieRepo extends JpaRepository<Movie,Long> {
 
+  List<Booking> findByMovieMovieId(Long movieId);
+    List<Booking> findByUserUserId(int userId);
 }
--- springapp/src/main/java/com/examly/springapp/service/MovieService.java
+++ springapp/src/main/java/com/examly/springapp/service/MovieService.java
@@ -1,5 +1,11 @@
 package com.examly.springapp.service;
+import java.util.List;
+import com.examly.springapp.entity.Movie;
 
 public interface MovieService {
-    
+    Movie addMovie(Movie movie);
+    List<Movie> getAllMovies();
+    Movie getMovieById(Long movieId);
+    Movie updateMovie(Long movieId, Movie movie);
+    boolean deleteMovie(Long movieId);
 }
