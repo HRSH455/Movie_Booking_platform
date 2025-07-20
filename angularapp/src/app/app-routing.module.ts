6789a5a461ddc93dc75d6d16a24d6b96e1d7483c
neojarvis <techadmin@iamneo.ai>
2025-07-20 07:26:51 +0000

    Submit-project-save-commit-7/20/2025--12:55:57-PM

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminaddmovieComponent } from './components/adminaddmovie/adminaddmovie.component';
+import { UserviewbookingComponent } from './components/userviewbooking/userviewbooking.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'admin/add/newMovies' ,component:AdminaddmovieComponent },
+  {path: 'user/view/Mybookings' ,component:UserviewbookingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
