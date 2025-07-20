9a03fbd52bf31330e07dca828af3963de2c8d2ce
neojarvis <techadmin@iamneo.ai>
2025-07-20 08:00:00 +0000

    Manual-save-commit-7/20/2025--1:29:08-PM

--- angularapp/src/app/components/registration/registration.component.ts
+++ angularapp/src/app/components/registration/registration.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent implements OnInit {
+export class RegistrationComponent {
+  registerForm: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]],
+      confirmPassword: ['', Validators.required],
+      mobileNumber: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
+      userRole: ['', Validators.required]
+    }, { validator: this.passwordMatchValidator });
   }
 
+  passwordMatchValidator(form: FormGroup) {
+    return form.get('password')?.value === form.get('confirmPassword')?.value 
+      ? null : { mismatch: true };
+  }
+
+  register() {
+    if (this.registerForm.valid) {
+      const user: User = {
+        username: this.registerForm.value.username,
+        email: this.registerForm.value.email,
+        password: this.registerForm.value.password,
+        mobileNumber: this.registerForm.value.mobileNumber,
+        userRole: this.registerForm.value.userRole
+      };
+
+      this.authService.register(user).subscribe(
+        response => {
+          this.successMessage = 'Registration successful! Please login.';
+          setTimeout(() => {
+            this.router.navigate(['/login']);
+          }, 2000);
+        },
+        error => {
+          this.errorMessage = 'Registration failed. Please try again.';
+        }
+      );
+    }
+  }
 }
